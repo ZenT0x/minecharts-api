1d4fc347f74ddf2b87f5210942581db920784a42
zentox <zentoxll@gmail.com>
2025-03-03 20:04:29 +0100

    Add PingHandler to API for health check endpoint

---
+++ cmd/api/ping.go
@@ -0,0 +1,8 @@
+package api
+
+import "github.com/gin-gonic/gin"
+
+// PingHandler returns a simple "pong" message to confirm the API is online.
+func PingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "pong"})
+}
