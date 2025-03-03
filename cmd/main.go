de364f74294faa33ae70b77edd791fdfb8ca26cb
zentox <zentoxll@gmail.com>
2025-03-03 20:04:26 +0100

    Refactor main.go to initialize Kubernetes client and set up API routes using Gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,84 +1,28 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"fmt"
-	"os"
-	"path/filepath"
+	"log"
 
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
+	"minecharts/cmd/api"
+	"minecharts/cmd/kubernetes"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	var configPath string
-
-	// Try to load kubeconfig from flag or default location
-	if home := os.Getenv("HOME"); home != "" {
-		configPath = filepath.Join(home, ".kube", "config")
-	}
-	flag.StringVar(&configPath, "kubeconfig", configPath, "absolute path to the kubeconfig file")
-	flag.Parse()
-
-	// Use kubeconfig if available, otherwise use in-cluster config
-	var config *rest.Config
-	var err error
-	if _, err := os.Stat(configPath); err == nil {
-		// kubeconfig exists, use it
-		config, err = clientcmd.BuildConfigFromFlags("", configPath)
-		if err != nil {
-			panic(err.Error())
-		}
-	} else {
-		// kubeconfig doesn't exist, assume in-cluster config
-		config, err = rest.InClusterConfig()
-		if err != nil {
-			panic(err.Error())
-		}
+	// Initialize the global Kubernetes clientset from the kubernetes package.
+	if err := kubernetes.Init(); err != nil {
+		log.Fatalf("Failed to initialize Kubernetes client: %v", err)
 	}
 
-	// Create the Kubernetes clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
+	// Create a new Gin router.
+	router := gin.Default()
 
-	// Define the pod specification
-	pod := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "minecraft-pod",
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:  "minecraft-server",
-					Image: "itzg/minecraft-server",
-					Env: []corev1.EnvVar{
-						{
-							Name:  "EULA",
-							Value: "TRUE",
-						},
-					},
-					Ports: []corev1.ContainerPort{
-						{
-							ContainerPort: 25565,
-							Protocol:      corev1.ProtocolTCP,
-						},
-					},
-				},
-			},
-		},
-	}
+	// Setup API routes.
+	api.SetupRoutes(router)
 
-	// Create the pod in the default namespace
-	podClient := clientset.CoreV1().Pods("default")
-	result, err := podClient.Create(context.TODO(), pod, metav1.CreateOptions{})
-	if err != nil {
-		panic(err.Error())
+	// Run the API server on port 8080.
+	if err := router.Run(":8080"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
 	}
-	fmt.Printf("Pod %q created.\n", result.Name)
 }
