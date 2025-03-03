44e1b0d3fa2abdbd72ee448520868afb2dce54b1
zentox <zentoxll@gmail.com>
2025-03-03 20:04:32 +0100

    Add handlers for listing and creating Minecraft pods in the API

---
+++ cmd/api/pods.go
@@ -0,0 +1,70 @@
+package api
+
+import (
+	"context"
+	"net/http"
+
+	"minecharts/cmd/kubernetes"
+
+	"github.com/gin-gonic/gin"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ListPodsHandler lists all pods in the "minecharts" namespace.
+func ListPodsHandler(c *gin.Context) {
+	pods, err := kubernetes.Clientset.CoreV1().Pods("minecharts").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, pods)
+}
+
+// CreateMinecraftPodHandler creates a new Minecraft pod in the "minecharts" namespace.
+// It expects a JSON body with a "podName" field.
+func CreateMinecraftPodHandler(c *gin.Context) {
+	var req struct {
+		PodName string `json:"podName"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	podName := "minecraft-server-" + req.PodName
+
+	// Define the pod with the itzg/minecraft-server image and accept the EULA.
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: podName,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "minecraft-server",
+					Image: "itzg/minecraft-server",
+					Env: []corev1.EnvVar{
+						{
+							Name:  "EULA",
+							Value: "TRUE",
+						},
+					},
+					Ports: []corev1.ContainerPort{
+						{
+							ContainerPort: 25565,
+							Protocol:      corev1.ProtocolTCP,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	newPod, err := kubernetes.Clientset.CoreV1().Pods("minecharts").Create(context.Background(), pod, metav1.CreateOptions{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, newPod)
+}
